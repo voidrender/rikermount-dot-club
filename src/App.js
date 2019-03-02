2bf3ab7d1925a2183c6a056ebbf4821b1ac46b66
Isaac Overacker <isaac@overacker.me>
2019-03-01 23:04:12 -0800

    Added stars!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import StarfieldAnimation from "react-starfield-animation";
+import "./App.css";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <StarfieldAnimation
+                    style={{
+                        position: "absolute",
+                        width: "100%",
+                        height: "100%"
+                    }}
+                    numParticles={1000}
+                    lineWidth={4}
+                    alphaFactor={1.0}
+                    depth={300}
+                />
                 <header className="App-header">
-                    <img src={require('./assets/mount-1.gif')} className="App-logo" alt="logo" />
+                    <img
+                        src={require("./assets/mount-1.gif")}
+                        className="App-logo"
+                        alt="logo"
+                    />
                 </header>
             </div>
         );
